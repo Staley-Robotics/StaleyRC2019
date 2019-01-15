aa4cfce36a0c05a16bb350115ec0a0e29d190613
Staley Robotics 4959 <staleyrobotics@gmail.com>
2019-01-15 17:00:13 -0600

    Pneumatics initial commit (#2)

---
+++ Staley RC 2019/src/main/java/frc/robot/commands/RunCompressor.java
@@ -0,0 +1,40 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.subsystems.Pneumatics;
+
+public class RunCompressor extends Command {
+
+    public RunCompressor() {
+        requires(Pneumatics.getInstance());
+    }
+
+    // Called just before this Command runs the first time
+    @Override
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    protected void execute() {
+        Pneumatics.getInstance().runCompressor();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    @Override
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    @Override
+    protected void interrupted() {
+    }
+
+}
