3e63371b2aa0db8545c4fe8b77267f1bdabfb243
Staley Robotics 4959 <staleyrobotics@gmail.com>
2019-01-15 15:36:05 -0600

    Drive (#1)
    * Initial drive code
    
    * Added pathfinder dependency

--- Staley RC 2019/src/main/java/frc/robot/OI.java
+++ Staley RC 2019/src/main/java/frc/robot/OI.java
@@ -7,36 +7,34 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-  //// CREATING BUTTONS
-  // One type of button is a joystick button which is any button on a
-  //// joystick.
-  // You create one by telling it which joystick it's on and which button
-  // number it is.
-  // Joystick stick = new Joystick(port);
-  // Button button = new JoystickButton(stick, buttonNumber);
+  private static OI instance;
 
-  // There are a few additional built in buttons you can use. Additionally,
-  // by subclassing Button you can create custom triggers and bind those to
-  // commands the same as any other Button.
+  private XboxController driveController;
 
-  //// TRIGGERING COMMANDS WITH BUTTONS
-  // Once you have a button, it's trivial to bind it to a button in one of
-  // three ways:
+  private OI() {
+    driveController = new XboxController(RobotMap.XBOX_PORT);
+  }
 
-  // Start the command when the button is pressed and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenPressed(new ExampleCommand());
+  public static OI getInstance() {
+    if(instance == null) {
+        instance = new OI();
+    }
+    return instance;
+  }
 
-  // Run the command while the button is being held down and interrupt it once
-  // the button is released.
-  // button.whileHeld(new ExampleCommand());
+  public double getDriveLeftY() {
+    return driveController.getY(Hand.kLeft);
+  }
 
-  // Start the command when the button is released and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenReleased(new ExampleCommand());
+  public double getDriveRightY() {
+    return driveController.getY(Hand.kRight);
+  }
 }
