58cd1f224da79be025b4c9c8af768c069a3c681f
StaleyRobotics <staleyrobotics@gmail.com>
2019-02-26 15:47:43 -0600

    Added the setPivots directly into OI, set the pivotStates to the dpad buttons, setCargo specifically is set to the start button rather than dpad. Also added runIntake and runOutput.

--- Staley RC 2019/src/main/java/frc/robot/OI.java
+++ Staley RC 2019/src/main/java/frc/robot/OI.java
@@ -8,33 +8,57 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.Relay.Direction;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.commands.auto.commands.DPadforreal;
 import frc.robot.commands.auto.commands.VisionTurning;
 import frc.robot.commands.drivetrain.ShifterToggle;
 import frc.robot.commands.hatch.HatchExtenderToggle;
 import frc.robot.commands.shooter.RunShooter;
+import frc.robot.commands.shooter.SetPivotAngle;
+import frc.robot.commands.shooter.ShootSequence;
+import frc.robot.enums.PivotTargets;
 import frc.robot.enums.XBoxButtons;
+import frc.robot.util.Constants;
+import frc.robot.util.DPadButton;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
+@SuppressWarnings("resource")
 public class OI {
   private static OI instance;
 
   private XboxController driveController;
   private XboxController altController;
 
+  // Commands used to set the targetAngle of the shooter pivot
+  private final Command setPivotGround;
+  private final Command setPivotLow;
+  private final Command setPivotMid;
+  private final Command setPivotHigh;
+  private final Command setPivotCargo;
+
   /**
-   * Sets driveController and altController. Sets the functions of each button for both controllers
+   * Sets driveController and altController. Sets the functions of each button for
+   * both controllers
    */
   private OI() {
-    //Drive controller
     driveController = new XboxController(RobotMap.XBOX_DRIVE_PORT);
-    //Alt controller
     altController = new XboxController(RobotMap.XBOX_ALT_PORT);
+
+    setPivotGround = new SetPivotAngle(PivotTargets.GROUND);
+    setPivotLow = new SetPivotAngle(PivotTargets.LOW);
+    setPivotMid = new SetPivotAngle(PivotTargets.MID);
+    setPivotHigh = new SetPivotAngle(PivotTargets.HIGH);
+    setPivotCargo = new SetPivotAngle(PivotTargets.CARGO);
+
+
+    // ***** Drive Controller Buttons *****
 
     // Toggle for shifting between high and low gear
     Button shifterToggle = new JoystickButton(driveController, XBoxButtons.kB.getValue());
@@ -44,42 +68,76 @@ public class OI {
     Button shiftToClimberToggle = new JoystickButton(driveController, XBoxButtons.kX.getValue());
     // shiftToClimberToggle.whenPressed(new ShiftToClimberToggle());
 
-    // Toggle for extending and retracting the rear lifter piston
-    Button rearLifterToggle = new JoystickButton(driveController, XBoxButtons.kY.getValue());
+    // Toggle for extending and retracting the rear lifter pistons
+    Button rearLifterToggle = new JoystickButton(driveController, XBoxButtons.kA.getValue());
     // rearLifterToggle.whenPressed(new RearLifterToggle());
 
-    // Toggle for extending and retracting the front lifter piston
-    Button frontLifterToggle = new JoystickButton(driveController, XBoxButtons.kA.getValue());
+    // Toggle for extending and retracting the front lifter pistons
+    Button frontLifterToggle = new JoystickButton(driveController, XBoxButtons.kY.getValue());
     // frontLifterToggle.whenPressed(new FrontLifterToggle());
+
+    Button turnToTape = new JoystickButton(driveController, XBoxButtons.kBumperLeft.getValue());
+    turnToTape.whenPressed(new VisionTurning());
+
+
+    // ***** Alt Controller Buttons *****
+
+    Button setGround = new JoystickButton(altController, XBoxButtons.kStart.getValue());
+    setGround.whenPressed(setPivotGround);
+    setGround.cancelWhenPressed(setPivotLow);
+    setGround.cancelWhenPressed(setPivotMid);
+    setGround.cancelWhenPressed(setPivotHigh);
+    setGround.cancelWhenPressed(setPivotCargo);
+
+    Button setLow = new DPadButton(altController, DPadButton.Direction.Down);
+    setLow.cancelWhenPressed(setPivotGround);
+    setLow.whenPressed(setPivotLow);
+    setLow.cancelWhenPressed(setPivotMid);
+    setLow.cancelWhenPressed(setPivotHigh);
+    setLow.cancelWhenPressed(setPivotCargo);
+
+    Button setMid = new DPadButton(altController, DPadButton.Direction.Right);
+    setMid.cancelWhenPressed(setPivotGround);
+    setMid.cancelWhenPressed(setPivotLow);
+    setMid.whenPressed(setPivotMid);
+    setMid.cancelWhenPressed(setPivotHigh);
+    setMid.cancelWhenPressed(setPivotCargo);
+
+    Button setHigh = new DPadButton(altController, DPadButton.Direction.Up);
+    setHigh.cancelWhenPressed(setPivotGround);
+    setHigh.cancelWhenPressed(setPivotLow);
+    setHigh.cancelWhenPressed(setPivotMid);
+    setHigh.whenPressed(setPivotHigh);
+    setHigh.cancelWhenPressed(setPivotCargo);
+
+    Button setCargo = new DPadButton(altController, DPadButton.Direction.Left);
+    setCargo.cancelWhenPressed(setPivotGround);
+    setCargo.cancelWhenPressed(setPivotLow);
+    setCargo.cancelWhenPressed(setPivotMid);
+    setCargo.cancelWhenPressed(setPivotHigh);
+    setCargo.whenPressed(setPivotCargo);
 
     // Toggle for extending and retracting hatch placer
     Button hatchToggle = new JoystickButton(altController, XBoxButtons.kX.getValue());
     hatchToggle.whenPressed(new HatchExtenderToggle());
 
-    Button runShooter = new JoystickButton(altController, XBoxButtons.kA.getValue());
-    hatchToggle.whileHeld(new RunShooter(0.4));
+    Button runIntake = new JoystickButton(altController, XBoxButtons.kA.getValue());
+    runIntake.whileHeld(new RunShooter(-0.4));
 
-    Button setPivotLow = new JoystickButton(altController, XBoxButtons.kUp.getValue());
-    // What thi supposed be setPivotLow.whenPressed(new Se);
-    
-    Button turnToTape = new JoystickButton(driveController, XBoxButtons.kBumperRight.getValue());
-    turnToTape.whenPressed(new VisionTurning());
+    // Button runShooter = new JoystickButton(altController, XBoxButtons.kB.getValue());
+    // runShooter.whenPressed(new ShootSequence());
 
-    Button pivotUp = new JoystickButton(altController, XBoxButtons.kBumperRight.getValue());
-    
-    Button pivotDown = new JoystickButton(altController, XBoxButtons.kBumperLeft.getValue());
+    Button runOutput = new JoystickButton(altController, XBoxButtons.kB.getValue());
+    runOutput.whileHeld(new RunShooter(0.4));
   }
 
-  /**
-   * Ensures that an instance of OI is received
-   * @return the instance of OI
-   */
   public static OI getInstance() {
     if (instance == null) {
       instance = new OI();
     }
     return instance;
   }
+
 
   // ***** Getters for X-Box controller(s) joystick axis and trigger values *****
 
@@ -109,6 +167,18 @@ public class OI {
     return driveController.getTriggerAxis(Hand.kRight);
   }
 
+  public boolean getDriveLeftBumper() {
+    return driveController.getBumper(Hand.kLeft);
+  }
+
+  public boolean getDriveRightBumper() {
+    return driveController.getBumper(Hand.kRight);
+  }
+
+  public int getDriveDPad() {
+    return driveController.getPOV();
+  }
+
   // Alternate controller
 
   public double getAltLeftX() {
@@ -133,5 +203,17 @@ public class OI {
 
   public double getAltRightTrigger() {
     return altController.getTriggerAxis(Hand.kRight);
+  }
+
+  public boolean getAltLeftBumper() {
+    return altController.getBumper(Hand.kLeft);
+  }
+
+  public boolean getAltRightBumper() {
+    return altController.getBumper(Hand.kRight);
+  }
+
+  public int getAltDPad() {
+    return altController.getPOV();
   }
 }
